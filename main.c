6c00b5a35d763f1445d7ff37d9db50c35ee373c5
zerechen <125591032@qq.com>
2020-04-30 14:45:23 +0000

    master

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
-  int x = 4;
-  int y = 5;
+int square(int a, int b){
+  int ret = a*a + b*b;
+  return ret;
+}
+
+int doCalc(int num1, int num2){
+  int ret = square(num1, num2);
+  return ret;
+}
+
+int main(){
+  int param1 = 1;
+  int param2 = 2;
+  int result = doCalc(param1,param2);
+  printf("result is%d\n",result);
 }
